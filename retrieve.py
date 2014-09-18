3797f55bab7aabe53aec598e9e002b292a21d750
Morris Jobke <hey@morrisjobke.de>
2014-09-18 22:00:46 +0200

    refactoring and add cache

--- retrieve.py
+++ retrieve.py
@@ -6,8 +6,14 @@ from collections import deque
 from helper.listConcat import listConcat
 
 from scrape import DateTimeEncoder
+import glob
 
-def findIntersection(street1, street2, street3=False):
+streetCache = {}
+
+def searchStreet(street):
+	if street in streetCache:
+		print('cache hit: %s '%street)
+		return streetCache[street]
 
 	a = '%3Cosm-script%20output%3D%22json%22%20timeout%3D%2225%22%3E%0A%20%20%20%20%3Cid-query%20type%3D%22area%22%20ref%3D%223600062594%22%20into%3D%22area%22%2F%3E%0A%20%20%20%20%3Cunion%3E%0A%20%20%20%20%20%20%20%20%3Cquery%20type%3D%22way%22%3E%0A%20%20%20%20%20%20%3Chas-kv%20k%3D%22name%22%20v%3D%22'
 	b = '%22%2F%3E%0A%20%20%20%20%20%20%3Carea-query%20from%3D%22area%22%2F%3E%0A%20%20%20%20%3C%2Fquery%3E%0A%20%20%3C%2Funion%3E%0A%20%20%3C%21--%20print%20results%20--%3E%0A%20%20%3Cprint%20mode%3D%22body%22%2F%3E%0A%20%20%3Crecurse%20type%3D%22down%22%2F%3E%0A%20%20%3Cprint%20mode%3D%22skeleton%22%20order%3D%22quadtile%22%2F%3E%0A%3C%2Fosm-script%3E'
@@ -27,84 +33,68 @@ def findIntersection(street1, street2, street3=False):
 		'</osm-script>' + \
 	'</osm-script>'
 
-	response = urllib.request.urlopen("http://overpass-api.de/api/interpreter?data=" + a + urllib.parse.quote(street1) + b )
+	response = urllib.request.urlopen("http://overpass-api.de/api/interpreter?data=" + a + urllib.parse.quote(street) + b )
 	content = response.read()
-	data1 = json.loads(content.decode('utf8'))
+	data = json.loads(content.decode('utf8'))
 
-	response = urllib.request.urlopen("http://overpass-api.de/api/interpreter?data=" + a + urllib.parse.quote(street2) + b )
-	content = response.read()
-	data2 = json.loads(content.decode('utf8'))
+	if len(data['elements']) == 0:
+		print('street %s couldn\'t be found'%street)
+		raise Exception
 
-	if street3:
-		response = urllib.request.urlopen("http://overpass-api.de/api/interpreter?data=" + a + urllib.parse.quote(street3) + b )
-		content = response.read()
-		data3 = json.loads(content.decode('utf8'))
-		if len(data3['elements']) == 0:
-			print('street3 couldn\'t be found')
-			return []
-
-	if len(data1['elements']) == 0:
-		print('street1 couldn\'t be found')
-		return []
-	if len(data2['elements']) == 0:
-		print('street2 couldn\'t be found')
-		return []
-
-	nodes1 = []
-	for d in data1['elements']:
+	nodes = []
+	ways = []
+	detailedNodes = {}
+	for d in data['elements']:
 		if d['type'] == 'way':
-			nodes1 = nodes1 + d['nodes']
+			nodes = nodes + d['nodes']
+			ways.append(d['nodes'])
+		if d['type'] == 'node':
+			detailedNodes[d['id']] = {'lng': d['lon'], 'lat': d['lat']}
 
-	if len(nodes1) == 0:
-		print('nodes1 empty')
+	if len(nodes) == 0:
+		print('no nodes for street %s'%street)
+		raise Exception
 
-	nodes2 = []
-	for d in data2['elements']:
-		if d['type'] == 'way':
-			nodes2 = nodes2 + d['nodes']
+	result = { 'nodes': set(nodes), 'detailed': detailedNodes, 'ways': ways }
 
-	if len(nodes2) == 0:
-		print('nodes2 empty')
+	streetCache[street] = result
 
-	if street3:
-		nodes3 = []
-		for d in data3['elements']:
-			if d['type'] == 'way':
-				nodes3 = nodes3 + d['nodes']
-		nodes3 = set(nodes3)
+	return result
 
-		if len(nodes3) == 0:
-			print('nodes3 empty')
+def findIntersection(street1, street2, street3=False):
 
-	nodes1 = set(nodes1)
-	nodes2 = set(nodes2)
+	try:
+		data1 = searchStreet(street1)
+		data2 = searchStreet(street2)
+	except Exception:
+		return []
 
-	sameNodes = list(nodes2.intersection(nodes1))
 
+	sameNodes = list(data2['nodes'].intersection(data1['nodes']))
 	if len(sameNodes) == 0:
 		print('sameNodes empty')
 
-	tmp = data1['elements'] + data2['elements']
-
-	if street3:
-		tmp = tmp + data3['elements']
-
 	allNodes = {}
-	for node in tmp:
-		if node['type'] == 'node':
-			allNodes[node['id']] = {'lat': node['lat'], 'lng': node['lon']}
+	allNodes.update(data1['detailed'])
+	allNodes.update(data2['detailed'])
 
 	result = []
 	if street3:
 		# specifc way
+		try:
+			data3 = searchStreet(street3)
+		except Exception:
+			return []
+
+		allNodes.update(data3['detailed'])
+
 		mergedWays = listConcat()
-		for d in data1['elements']:
-			if d['type'] == 'way':
-				mergedWays.add(d['nodes'])
+		for way in data1['ways']:
+			mergedWays.add(way)
 
 		ways = mergedWays.get()
 
-		sameNodes2 = list(nodes3.intersection(nodes1))
+		sameNodes2 = list(data3['nodes'].intersection(data3['nodes']))
 
 		if len(sameNodes2) == 0:
 			print('sameNodes2 empty')
@@ -115,15 +105,25 @@ def findIntersection(street1, street2, street3=False):
 					if node1 in way and node2 in way:
 						result.append([allNodes[n] for n in way])
 	else:
-		# specific point
-		for node in data2['elements']:
-			if node['type'] == 'node' and node['id'] in sameNodes:
-				result.append({'lat': node['lat'], 'lng': node['lon']})
+		for node in sameNodes:
+			result.append(allNodes[node])
 
 	return result
 
 def extract():
-	f = open('data-2014-05-16.10-25.json')
+	files = glob.glob('data-2*.json')
+	files.sort()
+	files.reverse()
+	if len(files) == 0:
+		raise SystemExit
+
+	# extract date
+	filename = files[0]
+	date = filename[5:-5]
+
+	print('file used: %s'%filename)
+
+	f = open(filename)
 	data = json.load(f)
 
 	found = []
@@ -157,12 +157,14 @@ def extract():
 		else:
 			print('%2i/%2i'%(i, len(data)))
 
-	f = open('data-parsed.json', 'w')
+	f = open('data-parsed-' + date + '.json', 'w')
 	json.dump(found, f, cls=DateTimeEncoder)
 	f.close()
-	f = open('data-parsed-notfound.json', 'w')
+	f = open('data-parsed-' + date + '-notfound.json', 'w')
 	json.dump(notfound, f, cls=DateTimeEncoder)
 	f.close()
+
+	print('stats: found: %2i/%2i'%(len(found), len(data)))
 
 if __name__ == "__main__":
 	extract()
