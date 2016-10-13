c7f5fcc3eebc64367a1d33b70730f186a9b46592
Morris Jobke <hey@morrisjobke.de>
2016-10-13 20:26:08 +0200

    reformat as pep8

--- extractors/street.py
+++ extractors/street.py
@@ -3,65 +3,68 @@
 
 import re
 
+
 class StreetExtractionException(Exception):
-	pass
+    pass
 
 
 def extract(locationString):
-	"""Return an dict with following keys: since, until, note(optional)
+    """Return an dict with following keys: since, until, note(optional)
 
-	>>> from pprint import pprint
-	>>> pprint(extract('zwischen Lange Straße und Leipziger Weg'))
-	{'relation': 'between', 'streets': ['Lange Straße', 'Leipziger Weg']}
+    >>> from pprint import pprint
+    >>> pprint(extract('zwischen Lange Straße und Leipziger Weg'))
+    {'relation': 'between', 'streets': ['Lange Straße', 'Leipziger Weg']}
 
-	>>> pprint(extract('Einmündung Lange Straße/Leipziger Weg'))
-	{'relation': 'intersection', 'streets': ['Lange Straße', 'Leipziger Weg']}
-	>>> pprint(extract('in Höhe Lange Straße/Leipziger Weg'))
-	{'relation': 'intersection', 'streets': ['Lange Straße', 'Leipziger Weg']}
-	>>> pprint(extract('Kreuzung Lange Straße/Leipziger Weg'))
-	{'relation': 'intersection', 'streets': ['Lange Straße', 'Leipziger Weg']}
+    >>> pprint(extract('Einmündung Lange Straße/Leipziger Weg'))
+    {'relation': 'intersection', 'streets': ['Lange Straße', 'Leipziger Weg']}
+    >>> pprint(extract('in Höhe Lange Straße/Leipziger Weg'))
+    {'relation': 'intersection', 'streets': ['Lange Straße', 'Leipziger Weg']}
+    >>> pprint(extract('Kreuzung Lange Straße/Leipziger Weg'))
+    {'relation': 'intersection', 'streets': ['Lange Straße', 'Leipziger Weg']}
 
-	>>> pprint(extract('stadtwärts nach Lange Straße'))
-	{'relation': 'townwards-after', 'streets': 'Lange Straße'}
-	>>> pprint(extract('landwärts vor Lange Straße'))
-	{'relation': 'townwards-after', 'streets': 'Lange Straße'}
+    >>> pprint(extract('stadtwärts nach Lange Straße'))
+    {'relation': 'townwards-after', 'streets': 'Lange Straße'}
+    >>> pprint(extract('landwärts vor Lange Straße'))
+    {'relation': 'townwards-after', 'streets': 'Lange Straße'}
 
-	>>> pprint(extract('stadtwärts vor Lange Straße'))
-	{'relation': 'townwards-before', 'streets': 'Lange Straße'}
-	>>> pprint(extract('landwärts nach Lange Straße'))
-	{'relation': 'townwards-before', 'streets': 'Lange Straße'}
-	>>> pprint(extract('landwärts Höhe Lange Straße'))
-	{'relation': 'townwards-before', 'streets': 'Lange Straße'}
+    >>> pprint(extract('stadtwärts vor Lange Straße'))
+    {'relation': 'townwards-before', 'streets': 'Lange Straße'}
+    >>> pprint(extract('landwärts nach Lange Straße'))
+    {'relation': 'townwards-before', 'streets': 'Lange Straße'}
+    >>> pprint(extract('landwärts Höhe Lange Straße'))
+    {'relation': 'townwards-before', 'streets': 'Lange Straße'}
 
-	"""
+    """
 
-	data = {}
-	between = re.match('^zwischen\s*(.*) und (.*)\s*$', locationString)
-	if between:
-		data['streets'] = list(between.groups())
-		data['relation'] = 'between'
-		return data
+    data = {}
+    between = re.match('^zwischen\s*(.*) und (.*)\s*$', locationString)
+    if between:
+        data['streets'] = list(between.groups())
+        data['relation'] = 'between'
+        return data
 
-	intersection = re.match('^(Einmündung|in Höhe|Kreuzung)\s*(.*)\s*$', locationString)
-	if intersection:
-		data['streets'] = intersection.group(2).split('/')
-		data['relation'] = 'intersection'
-		return data
+    intersection = re.match('^(Einmündung|in Höhe|Kreuzung)\s*(.*)\s*$', locationString)
+    if intersection:
+        data['streets'] = intersection.group(2).split('/')
+        data['relation'] = 'intersection'
+        return data
 
-	townwards = re.match('^(stadtwärts nach|landwärts vor)\s*(.*)\s*$', locationString)
-	if townwards:
-		data['streets'] = townwards.group(2)
-		data['relation'] = 'townwards-after'
-		return data
+    townwards = re.match('^(stadtwärts nach|landwärts vor)\s*(.*)\s*$', locationString)
+    if townwards:
+        data['streets'] = townwards.group(2)
+        data['relation'] = 'townwards-after'
+        return data
 
-	countrywards = re.match('^(stadtwärts vor|landwärts nach|landwärts Höhe)\s*(.*)\s*$', locationString)
-	if countrywards:
-		data['streets'] = countrywards.group(2)
-		data['relation'] = 'townwards-before'
-		return data
+    countrywards = re.match('^(stadtwärts vor|landwärts nach|landwärts Höhe)\s*(.*)\s*$', locationString)
+    if countrywards:
+        data['streets'] = countrywards.group(2)
+        data['relation'] = 'townwards-before'
+        return data
 
-	raise StreetExtractionException(locationString)
+    raise StreetExtractionException(locationString)
+
 
 if __name__ == "__main__":
-	import doctest
-	doctest.testmod()
+    import doctest
+
+    doctest.testmod()
--- retrieve.py
+++ retrieve.py
@@ -10,177 +10,179 @@ import glob
 
 streetCache = {}
 
+
 def searchStreet(street):
-	if street in streetCache:
-		print('cache hit: %s '%street)
-		return streetCache[street]
+    if street in streetCache:
+        print('cache hit: %s ' % street)
+        return streetCache[street]
 
-	data = '<osm-script output="json" timeout="25">' + \
-		'<id-query into="area" ref="3600062594" type="area"/>' + \
-		'<difference>' + \
-                    '<query type="way">' + \
-                        '<has-kv k="name" v="PLACEHOLDER"/>' + \
-                        '<has-kv k="highway" />' + \
-                        '<area-query from="area"/>' + \
-                    '</query>' + \
-                    '<union>' + \
-                        '<query type="way">' + \
-                            '<has-kv k="highway" v="service"/>' + \
-                            '<area-query from="area"/>' + \
-                        '</query>' + \
-                        '<query type="way">' + \
-                            '<has-kv k="highway" v="track"/>' + \
-                            '<area-query from="area"/>' + \
-                        '</query>' + \
-                    '</union>' + \
-		'</difference>' + \
-		'<print mode="body"/>' + \
-		'<recurse type="down"/>' + \
-		'<print mode="skeleton" order="quadtile"/>' + \
-	'</osm-script>'
+    data = '<osm-script output="json" timeout="25">' + \
+           '<id-query into="area" ref="3600062594" type="area"/>' + \
+           '<difference>' + \
+           '<query type="way">' + \
+           '<has-kv k="name" v="PLACEHOLDER"/>' + \
+           '<has-kv k="highway" />' + \
+           '<area-query from="area"/>' + \
+           '</query>' + \
+           '<union>' + \
+           '<query type="way">' + \
+           '<has-kv k="highway" v="service"/>' + \
+           '<area-query from="area"/>' + \
+           '</query>' + \
+           '<query type="way">' + \
+           '<has-kv k="highway" v="track"/>' + \
+           '<area-query from="area"/>' + \
+           '</query>' + \
+           '</union>' + \
+           '</difference>' + \
+           '<print mode="body"/>' + \
+           '<recurse type="down"/>' + \
+           '<print mode="skeleton" order="quadtile"/>' + \
+           '</osm-script>'
 
-	data = data.replace('PLACEHOLDER', street)
+    data = data.replace('PLACEHOLDER', street)
 
-	response = urllib.request.urlopen("http://overpass-api.de/api/interpreter?data=" + urllib.parse.quote_plus(data))
-	content = response.read()
-	data = json.loads(content.decode('utf8'))
+    response = urllib.request.urlopen("http://overpass-api.de/api/interpreter?data=" + urllib.parse.quote_plus(data))
+    content = response.read()
+    data = json.loads(content.decode('utf8'))
 
-	if len(data['elements']) == 0:
-		print('street %s couldn\'t be found'%street)
-		raise Exception
+    if len(data['elements']) == 0:
+        print('street %s couldn\'t be found' % street)
+        raise Exception
 
-	nodes = []
-	ways = []
-	detailedNodes = {}
-	for d in data['elements']:
-		if d['type'] == 'way':
-			nodes = nodes + d['nodes']
-			ways.append(d['nodes'])
-		if d['type'] == 'node':
-			detailedNodes[d['id']] = {'lng': d['lon'], 'lat': d['lat']}
+    nodes = []
+    ways = []
+    detailedNodes = {}
+    for d in data['elements']:
+        if d['type'] == 'way':
+            nodes = nodes + d['nodes']
+            ways.append(d['nodes'])
+        if d['type'] == 'node':
+            detailedNodes[d['id']] = {'lng': d['lon'], 'lat': d['lat']}
 
-	if len(nodes) == 0:
-		print('no nodes for street %s'%street)
-		raise Exception
+    if len(nodes) == 0:
+        print('no nodes for street %s' % street)
+        raise Exception
 
-	result = { 'nodes': set(nodes), 'detailed': detailedNodes, 'ways': ways }
+    result = {'nodes': set(nodes), 'detailed': detailedNodes, 'ways': ways}
 
-	streetCache[street] = result
+    streetCache[street] = result
 
-	return result
+    return result
+
 
 def findIntersection(street1, street2, street3=False):
+    try:
+        data1 = searchStreet(street1)
+        data2 = searchStreet(street2)
+    except Exception:
+        return []
 
-	try:
-		data1 = searchStreet(street1)
-		data2 = searchStreet(street2)
-	except Exception:
-		return []
+    sameNodes = list(data2['nodes'].intersection(data1['nodes']))
+    if len(sameNodes) == 0:
+        print('sameNodes empty')
 
+    allNodes = {}
+    allNodes.update(data1['detailed'])
+    allNodes.update(data2['detailed'])
 
-	sameNodes = list(data2['nodes'].intersection(data1['nodes']))
-	if len(sameNodes) == 0:
-		print('sameNodes empty')
+    result = []
+    if street3:
+        # specifc way
+        try:
+            data3 = searchStreet(street3)
+        except Exception:
+            return []
 
-	allNodes = {}
-	allNodes.update(data1['detailed'])
-	allNodes.update(data2['detailed'])
+        allNodes.update(data3['detailed'])
 
-	result = []
-	if street3:
-		# specifc way
-		try:
-			data3 = searchStreet(street3)
-		except Exception:
-			return []
+        mergedWays = listConcat()
+        for way in data1['ways']:
+            mergedWays.add(way)
 
-		allNodes.update(data3['detailed'])
+        ways = mergedWays.get()
 
-		mergedWays = listConcat()
-		for way in data1['ways']:
-			mergedWays.add(way)
+        sameNodes2 = list(data3['nodes'].intersection(data3['nodes']))
 
-		ways = mergedWays.get()
+        if len(sameNodes2) == 0:
+            print('sameNodes2 empty')
 
-		sameNodes2 = list(data3['nodes'].intersection(data3['nodes']))
+        for node1 in sameNodes:
+            for node2 in sameNodes2:
+                for way in ways:
+                    if node1 in way and node2 in way:
+                        result.append([allNodes[n] for n in way])
+    else:
+        for node in sameNodes:
+            result.append(allNodes[node])
 
-		if len(sameNodes2) == 0:
-			print('sameNodes2 empty')
+    return result
 
-		for node1 in sameNodes:
-			for node2 in sameNodes2:
-				for way in ways:
-					if node1 in way and node2 in way:
-						result.append([allNodes[n] for n in way])
-	else:
-		for node in sameNodes:
-			result.append(allNodes[node])
-
-	return result
 
 def findStreet(street):
-	data = searchStreet(street)
-	result = []
-	for way in data['ways']:
-		result.append([data['detailed'][n] for n in way])
-	return result
+    data = searchStreet(street)
+    result = []
+    for way in data['ways']:
+        result.append([data['detailed'][n] for n in way])
+    return result
 
 
 def extract():
-	files = glob.glob('data-2*.json')
-	files.sort()
-	files.reverse()
-	if len(files) == 0:
-		raise SystemExit
+    files = glob.glob('data-2*.json')
+    files.sort()
+    files.reverse()
+    if len(files) == 0:
+        raise SystemExit
 
-	# extract date
-	filename = files[0]
-	date = filename[5:-5]
+    # extract date
+    filename = files[0]
+    date = filename[5:-5]
 
-	print('file used: %s'%filename)
+    print('file used: %s' % filename)
 
-	f = open(filename)
-	data = json.load(f)
+    f = open(filename)
+    data = json.load(f)
 
-	found = []
-	notfound = []
-	i = 0
-	for entry in data:
-		i += 1
-		if 'location' in entry['parsed']:
-			street1 = entry['street']
-			street2 = entry['parsed']['location']['streets'][0]
-			# remove "
-			street2 = street2.replace('"', '')
-			if entry['parsed']['location']['relation'] == 'intersection':
-				print('%2i/%2i process - intersection - %s %s'%(i, len(data), street1, street2))
-				geodata = findIntersection(street1, street2)
-			elif entry['parsed']['location']['relation'] == 'between':
-				street3 = entry['parsed']['location']['streets'][1]
-				print('%2i/%2i process - between - %s %s %s'%(i, len(data), street1, street2, street3))
-				geodata = findIntersection(street1, street2, street3)
-			else:
-				print('%2i/%2i skip - %s'%(i, len(data), entry['parsed']['location']['relation']))
-				continue
+    found = []
+    notfound = []
+    i = 0
+    for entry in data:
+        i += 1
+        if 'location' in entry['parsed']:
+            street1 = entry['street']
+            street2 = entry['parsed']['location']['streets'][0]
+            # remove "
+            street2 = street2.replace('"', '')
+            if entry['parsed']['location']['relation'] == 'intersection':
+                print('%2i/%2i process - intersection - %s %s' % (i, len(data), street1, street2))
+                geodata = findIntersection(street1, street2)
+            elif entry['parsed']['location']['relation'] == 'between':
+                street3 = entry['parsed']['location']['streets'][1]
+                print('%2i/%2i process - between - %s %s %s' % (i, len(data), street1, street2, street3))
+                geodata = findIntersection(street1, street2, street3)
+            else:
+                print('%2i/%2i skip - %s' % (i, len(data), entry['parsed']['location']['relation']))
+                continue
 
-			if len(geodata) > 0:
-				print('\tfound')
-				entry['geodata'] = geodata
-				found.append(entry)
-			else:
-				print('\tnot found')
-				notfound.append(entry)
-		else:
-			print('%2i/%2i'%(i, len(data)))
+            if len(geodata) > 0:
+                print('\tfound')
+                entry['geodata'] = geodata
+                found.append(entry)
+            else:
+                print('\tnot found')
+                notfound.append(entry)
+        else:
+            print('%2i/%2i' % (i, len(data)))
 
-	f = open('data-parsed-' + date + '.json', 'w')
-	json.dump(found, f, cls=DateTimeEncoder)
-	f.close()
-	f = open('data-parsed-' + date + '-notfound.json', 'w')
-	json.dump(notfound, f, cls=DateTimeEncoder)
-	f.close()
+    f = open('data-parsed-' + date + '.json', 'w')
+    json.dump(found, f, cls=DateTimeEncoder)
+    f.close()
+    f = open('data-parsed-' + date + '-notfound.json', 'w')
+    json.dump(notfound, f, cls=DateTimeEncoder)
+    f.close()
 
-	print('stats: found: %2i/%2i'%(len(found), len(data)))
+    print('stats: found: %2i/%2i' % (len(found), len(data)))
+
 
 if __name__ == "__main__":
-	extract()
+    extract()
